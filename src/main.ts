ab421db630af7d421870e3766db30ef67ea7cfed
Divine <divywealth1@gmail.com>
2024-05-30 03:21:13 +0100

    Added patch to cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       "http://localhost:5173",
+      "https://propertyprospecthq.netlify.app"
     ],
     methods: ['POST', 'PUT', 'DELETE', 'GET', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
